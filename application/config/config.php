b1b8954065035c7599dd4f9974d6b9b3b0260b20
letyciap <letycia.mendes@escolar.ifrn.edu.br>
2021-12-13 15:55:07 -0300

    correções 4

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://projetoapirest.herokuapp.com/';
-$config['base_url'] = 'https://apirest-poo.herokuapp.com/';
+$config['base_url'] = 'https://letycia-apirest-poo.herokuapp.com/';
 //$config['base_url'] = 'https://localhost/apirest_poo/';
 
 /*
--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-BASE_URL = "https://apirest-poo.herokuapp.com/"
+BASE_URL = "https://letycia-apirest-poo.herokuapp.com/"
 //BASE_URL = "https://localhost/projeto_apirest/"
